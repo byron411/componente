e55faa39cc23ffaf3b8421abcbb3d647309cfca4
unknown <byron789_@hotmail.com>
2021-10-08 15:06:03 -0500

    se borro método repetido y se terminó esta clase

--- fabricaDAO/PostgreSQLPaisDAO.cs
+++ fabricaDAO/PostgreSQLPaisDAO.cs
@@ -178,27 +178,64 @@ namespace fabricaDAO
                 }
             }
         }
-        public List<PaisDTO> selectAll()
-        {
-            return null;
-        }
-        public PaisDTO selectByPK(PaisDTO pPais)
-        {
-            return null;
-        }
-        public List<PaisDTO> selectByCriteria(string pIdPais, string pNombre, string pPresidente)
-        {
-            return null;
-        }
-
-        //Métodos propios
 
         /*
-         * Devuelve un pais dado su id
-         * @return pais con el id dado null en caso contrario
+         * Devuelve una lista de todos los países en la base de datos
+         * @return lista de objetos de tipo PaisDTO, lista vacia si no hay paises
          */
+        public List<PaisDTO> selectAll()
+        {
+            try
+            {
+                List<PaisDTO> lista = new List<PaisDTO>();
+                DataSet conjunto = new DataSet();
 
-        public PaisDTO selectByPk(PaisDTO pPais)
+                // TODO Completar método para llamar al procedimiento que devuelve todos los países
+                _comando.CommandText = "seleccionarPais";
+                _comando.Parameters.Clear();
+                _adaptador.Fill(conjunto);
+                //
+                for (int i = 0; i < conjunto.Tables[0].Rows.Count; i++)
+                {
+                    PaisDTO pais = new PaisDTO();
+
+                    pais.AnioIndependencia = Convert.ToInt32(conjunto.Tables[0].Rows[i]["anioIndependencia"]);
+                    pais.Continente = conjunto.Tables[0].Rows[i]["continente"] + "";
+                    pais.EsperanzaVida = Convert.ToInt32(conjunto.Tables[0].Rows[i]["esperanzaVida"]);
+                    pais.FormaGobierno = conjunto.Tables[0].Rows[i]["formaGobierno"] + "";
+                    pais.IdPais = conjunto.Tables[0].Rows[i]["codigo_pais"] + "";
+                    pais.Nombre = conjunto.Tables[0].Rows[i]["nombre"] + "";
+                    pais.Pib = Convert.ToDouble(conjunto.Tables[0].Rows[i]["pib"]);
+                    pais.Poblacion = Convert.ToInt32(conjunto.Tables[0].Rows[i]["poblacion"]);
+                    pais.Presidente = conjunto.Tables[0].Rows[i]["presidente"] + "";
+                    pais.Region = conjunto.Tables[0].Rows[i]["region"] + "";
+                    pais.Superficie = Convert.ToDouble(conjunto.Tables[0].Rows[i]["superficie"]);
+
+                    lista.Add(pais);
+                }
+                return lista;
+            }
+            catch (Exception error)
+            {
+
+                throw new Exception(error.Message);
+            }
+            finally
+            {
+                if (_conexion.State == ConnectionState.Open)
+                {
+                    _conexion.Close();
+                }
+            }
+        }
+
+        /*
+         * Busca un país por llave primaria
+         * @pPais objeto PaisDTO para acceder a la id y buscarlo 
+         * @except lanza una excepción si no se logra conectar
+         * @return objeto de tipo PasiDTO null si no lo encuentra
+         */
+        public PaisDTO selectByPK(PaisDTO pPais)
         {
             try
             {
@@ -247,6 +284,65 @@ namespace fabricaDAO
         }
 
         /*
+         * Devuelve un país buscando por id, nombre y presidente
+         * @param pIdPais identificación del país a buscar
+         * @param pNombre nombre del país a buscar
+         * @param pPresidente presidente del pais a buscar
+         * @except lanza un error si hay un error conectandose
+         * @return objeto de tipo PaisDTO, null si no lo encuentra
+         */
+        public List<PaisDTO> selectByCriteria(string pIdPais, string pNombre, string pPresidente)
+        {
+            try
+            {
+                List<PaisDTO> lista = new List<PaisDTO>();
+                DataSet conjunto = new DataSet();
+
+                // TODO Completar para llamar al procedimiento que devuelve los países de acuerdo con los parámetros dados
+                _comando.CommandText = "seleccionarPaisPorCriterio";
+                _comando.Parameters.Clear();
+                _comando.Parameters.AddWithValue("pIdPais", pIdPais);
+                _comando.Parameters.AddWithValue("pNombre", pNombre);
+                _comando.Parameters.AddWithValue("pPresidente", pPresidente);
+                _adaptador.Fill(conjunto);
+                //
+                for (int i = 0; i < conjunto.Tables[0].Rows.Count; i++)
+                {
+                    PaisDTO pais = new PaisDTO();
+
+                    pais.AnioIndependencia = Convert.ToInt32(conjunto.Tables[0].Rows[i]["anioIndependencia"]);
+                    pais.Continente = conjunto.Tables[0].Rows[i]["continente"] + "";
+                    pais.EsperanzaVida = Convert.ToInt32(conjunto.Tables[0].Rows[i]["esperanzaVida"]);
+                    pais.FormaGobierno = conjunto.Tables[0].Rows[i]["formaGobierno"] + "";
+                    pais.IdPais = conjunto.Tables[0].Rows[i]["codigo_pais"] + "";
+                    pais.Nombre = conjunto.Tables[0].Rows[i]["nombre"] + "";
+                    pais.Pib = Convert.ToDouble(conjunto.Tables[0].Rows[i]["pib"]);
+                    pais.Poblacion = Convert.ToInt32(conjunto.Tables[0].Rows[i]["poblacion"]);
+                    pais.Presidente = conjunto.Tables[0].Rows[i]["presidente"] + "";
+                    pais.Region = conjunto.Tables[0].Rows[i]["region"] + "";
+                    pais.Superficie = Convert.ToDouble(conjunto.Tables[0].Rows[i]["superficie"]);
+
+                    lista.Add(pais);
+                }
+                return lista;
+            }
+            catch (Exception error)
+            {
+
+                throw new Exception(error.Message);
+            }
+            finally
+            {
+                if (_conexion.State == ConnectionState.Open)
+                {
+                    _conexion.Close();
+                }
+            }
+        }
+
+        
+
+        /*
          * Devuelve un país dado su nombre
          * @return el pais con el nombre dado null en caso contrario
          */
@@ -282,13 +378,16 @@ namespace fabricaDAO
             return pais;
         }
 
+
+        //Métodos propios
+
         /*
          *Devuelve la cantidad de ciudades registradas por país
          * @param pIdPais elemento string que se recibe y se desea buscar
          * @exception lanza una excepción si ha ocurrido algún error en la base de datos
          * @return duvuelve 0 si no encuentra una ciudad correspondiente a ese pais
          */
-         public int selectCiudadesByPais(String pIdPais)
+        public int selectCiudadesByPais(String pIdPais)
         {
             try
             {
